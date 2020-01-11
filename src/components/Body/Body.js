e0f33ba4ab93bee3f70b5a2a4992a01493d8b305
BenLirio <benjlirio@gmail.com>
2020-01-10 20:38:41 -0500

    Change to scss

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import classes from './Body.module.css'
+import classes from './Body.module.scss'
 export default function Body() {
   return <div className={classes.Body}></div>
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import classes from './Footer.module.css'
+import classes from './Footer.module.scss'
 
 export default function Footer() {
   return <div className={classes.Footer}></div>
