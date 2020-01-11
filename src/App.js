9e143524d8d6badf2db816ad462376e896b5c661
BenLirio <benjlirio@gmail.com>
2020-01-10 19:55:41 -0500

    Import layout children into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import classes from './App.module.css'
+import Header from './components/Header/Header'
+import Body from './components/Body/Body'
+import Footer from './components/Footer/Footer'
 export default class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className={classes.App}>
         <Header />
         <Body />
         <Footer />
