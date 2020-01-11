fdde5587f4dff54602261eb0c834dc8d07ae3c86
BenLirio <benjlirio@gmail.com>
2020-01-10 21:05:40 -0500

    Add topbar

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -4,7 +4,7 @@ import classes from './TopBar.module.scss'
 export default function TopBar() {
   return (
     <div className={classes.TopBar}>
-      <div>TopBar</div>
+      <div></div>
     </div>
   )
 }
