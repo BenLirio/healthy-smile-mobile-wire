4acf6ccf7735676ba483dbc87759ffde21e6739c
BenLirio <benjlirio@gmail.com>
2020-01-11 13:49:25 -0500

    In the midde ..

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,7 +3,7 @@ import classes from './Text.module.scss'
 export default function Text() {
   return (
     <div className={classes.Text}>
-      <h2>title</h2>
+      <h4>title</h4>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, rerum?
       </p>
