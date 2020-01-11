9aa0cf78ec81d367a508600ddb928a1b9aa841fa
BenLirio <benjlirio@gmail.com>
2020-01-10 21:19:20 -0500

    Add cta

---
+++ src/components/Cta/Cta.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './Cta.module.scss'
+export default function Cta() {
+  return (
+    <div className={classes.Cta}>
+      <div>Cta</div>
+    </div>
+  )
+}
--- src/containers/Body/Body.js
+++ src/containers/Body/Body.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import classes from './Body.module.scss'
+import Cta from '../../components/Cta/Cta'
 export default function Body() {
-  return <div className={classes.Body}></div>
+  return (
+    <div className={classes.Body}>
+      <Cta />
+    </div>
+  )
 }
