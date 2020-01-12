a3236587edd05904b727581c026600b5bdb604db
BenLirio <benjlirio@gmail.com>
2020-01-12 11:46:33 -0500

    Change app js to render home container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react'
-import classes from './App.module.css'
-import Header from './containers/Header/Header'
-import Body from './containers/Body/Body'
-import Footer from './containers/Footer/Footer'
+import Home from './containers/Home/Home'
 export default class App extends Component {
   render() {
-    return (
-      <div className={classes.App}>
-        <Header />
-        <Body />
-        <Footer />
-      </div>
-    )
+    return <Home></Home>
   }
 }
