d83b12592c1bb67bb19502b12e20954730d58ca6
BenLirio <benjlirio@gmail.com>
2020-01-12 12:13:35 -0500

    Add image container

--- src/components/Sections/Header/Header.js
+++ src/components/Sections/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Aux from '../../../Hoc/Aux/Aux'
+import TopNav from '../../Nav/TopNav/TopNav'
+import ImageContainer from '../../ImageContainer/ImageContainer'
 
 export default function Header() {
   return (
-    <div>
-      <div>header</div>
-    </div>
+    <Aux>
+      <TopNav />
+      <ImageContainer />
+    </Aux>
   )
 }
