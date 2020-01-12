71b8bb7aeab6ebe12704d885c64e099fa176d546
BenLirio <benjlirio@gmail.com>
2020-01-12 17:36:55 -0500

    Clean up app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Header, Body, Footer } from './Sections/index'
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Header />
-        <Body />
-        <Footer />
-      </Router>
-    )
-  }
-}
+export default () => (
+  <Router>
+    <Header />
+    <Body />
+    <Footer />
+  </Router>
+)
