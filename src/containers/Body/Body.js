d685a6e97d8c741fab1ef0ddb152a130d7143539
BenLirio <benjlirio@gmail.com>
2020-01-11 19:46:31 -0500

    Complete Review

--- src/containers/Body/Body.js
+++ src/containers/Body/Body.js
@@ -5,11 +5,18 @@ import Section from '../../components/Section/Section'
 import Post from '../../components/Post/Post'
 import Cta from '../../components/Cta/Cta'
 import Feature from '../../components/Feature/Feature'
+import ReviewHolder from '../../components/ReviewHolder/ReviewHolder'
+import Review from '../../components/Review/Review'
 
 export default function Body() {
   return (
     <div className={classes.Body}>
       <Post>
+        <ReviewHolder>
+          <Review />
+          <Review />
+          <Review />
+        </ReviewHolder>
         <Section>
           <Feature />
           <Feature />
