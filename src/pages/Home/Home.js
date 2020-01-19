68fe05fb7a1896285223f0464d4a8b34b755f80b
BenLirio <benjlirio@gmail.com>
2020-01-19 13:04:43 -0500

    General layout for home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,83 @@
 import React from 'react'
-import classes from './Home.module.scss'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import FeatureImage from '../../components/FeatureImage/FeatureImage'
-import Feature from '../../components/Feature/Feature'
-import Testimonials from '../../components/Testimonials/Testimonials'
-import About from '../../components/About/About'
-import Cta from '../../components/Cta/Cta'
-import OurDoctors from '../../components/OurDoctors/OurDoctors'
-import Maps from '../../components/Maps/Maps'
+
+const Blank = () => (
+  <div
+    style={{
+      height: '100px',
+      border: '2px solid #000',
+      background: '#292',
+    }}></div>
+)
+const Blank2 = () => (
+  <div
+    style={{
+      height: '200px',
+      border: '2px solid #000',
+      background: '#292',
+    }}></div>
+)
+const Blank3 = () => (
+  <div
+    style={{
+      height: '300px',
+      border: '2px solid #000',
+      background: '#292',
+    }}></div>
+)
+const Blank4 = () => (
+  <div
+    style={{
+      height: '400px',
+      border: '2px solid #000',
+      background: '#292',
+    }}></div>
+)
 
 export const Home = () => (
-  <main className={classes.Main}>
-    <FeatureImage />
-    <Feature />
-    <Feature />
-    <Feature />
-    <Testimonials />
-    <About />
-    <Cta />
-    <OurDoctors />
-    <Maps />
-  </main>
+  <Container>
+    <Row>
+      <Col>
+        <FeatureImage />
+      </Col>
+    </Row>
+    <Row noGutters>
+      <Col>
+        <Blank2 />
+      </Col>
+      <Col>
+        <Blank2 />
+      </Col>
+      <Col>
+        <Blank2 />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Blank3 />
+      </Col>
+    </Row>
+    <Row noGutters>
+      <Col xs={4}>
+        <Blank4 />
+      </Col>
+      <Col xs={8}>
+        <Blank4 />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Blank2 />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Blank4 />
+      </Col>
+    </Row>
+  </Container>
 )
